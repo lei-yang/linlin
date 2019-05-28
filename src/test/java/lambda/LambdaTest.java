a4ddfdcb5256fe3bf3483d0d8bfb73527792b0d9
leiyang <leiyang@163.com>
2019-05-28 11:03:26 +0800

    升级版本

---
+++ src/test/java/lambda/LambdaTest.java
@@ -0,0 +1,16 @@
+package lambda;
+
+import org.junit.Test;
+
+public class LambdaTest {
+
+    @Test
+    public void testFun() {
+        int x = 5;
+        int y = 6;
+    }
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
